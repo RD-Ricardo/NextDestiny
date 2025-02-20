18227201730141743c06106b776f07f4b0b99f73
RD-Ricardo <ricardodias169@gmail.com>
2025-02-20 17:53:24 -0300

    feat(catalog): implementation repositories

---
+++ src/Catalog/Catalog.Infrastructure/InfrastructureModule.cs
@@ -0,0 +1,27 @@
+﻿using Catalog.Application.Services;
+using Catalog.Application.Services.Interfaces;
+using Catalog.Domain.Repositories;
+using Catalog.Infrastructure.Persistence;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NextDestiny.Core.Database.MongoDb;
+
+namespace Catalog.Infrastructure
+{
+    public static class InfrastructureModule
+    {
+        public static IServiceCollection AddInfraCatalog(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAutoMapper(typeof(Application.Mappers.ProductProfile));
+         
+            services.AddMongoDb(configuration);
+            
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+            // Services
+            services.AddScoped<IProductService, ProductService>();
+
+            return services;
+        }
+    }
+}
---
+++ src/Catalog/Catalog.Infrastructure/Persistence/ProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Driver;
+using Catalog.Domain.Entities;
+using Catalog.Domain.Repositories;
+
+namespace Catalog.Infrastructure.Persistence
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IMongoCollection<Product> _productCollection;
+
+        public ProductRepository(IMongoDatabase database)
+        {
+            _productCollection = database.GetCollection<Product>("products");
+        }
+
+        public async Task CreateAsync(Product product, CancellationToken cancellationToken)
+        {
+            await _productCollection.InsertOneAsync(product, null, cancellationToken);
+        }
+
+        public async Task<List<Product>> GetAllAsync(CancellationToken cancellationToken)
+        {
+            var products = await _productCollection.Find(_ => true).ToListAsync(cancellationToken);
+            return products;
+        }
+    }
+}

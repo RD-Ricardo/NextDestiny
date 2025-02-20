bcc2b147d5b1760e9bd6c220926084f6d0af0d69
RD-Ricardo <ricardodias169@gmail.com>
2025-02-20 17:34:50 -0300

    feat: baseEntity

---
+++ building blocks/NextDestiny.Core/DomainObjects/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace NextDestiny.Core.DomainObjects
+{
+    public abstract class BaseEntity
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.String)]
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; } = null;
+        
+        public BaseEntity()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}

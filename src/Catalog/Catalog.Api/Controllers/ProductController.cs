272cef15e3fc31190ef2969bed274add4d8fc487
RD-Ricardo <ricardodias169@gmail.com>
2025-02-20 17:49:40 -0300

    feat(catalog): api

---
+++ src/Catalog/Catalog.Api/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using Catalog.Application.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using Catalog.Application.Services.Interfaces;
+
+namespace Catalog.Api.Controllers
+{
+    [ApiController]
+    [Route("api/products")]
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(ProductCreateDto dto, CancellationToken cancellationToken)
+        {
+            var result = await _productService.CreateProductAsync(dto, cancellationToken);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts(CancellationToken cancellationToken)
+        {
+            var result = await _productService.GetProductsAsync(cancellationToken);
+            return Ok(result);
+        }
+    }
+}

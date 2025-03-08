efbf94969ebb353a5e1ca75b9b8d7c49a3ef33a8
RD-Ricardo <ricardodias169@gmail.com>
2025-03-07 21:57:33 -0300

    fix: build and add k8s

--- src/Services/Order/Order.Api/Program.cs
+++ src/Services/Order/Order.Api/Program.cs
@@ -1,18 +1,16 @@
+using NextDestiny.Core.SecretManager;
 using Order.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+builder.Configuration.AddSecretManager();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddInfraOrder(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
